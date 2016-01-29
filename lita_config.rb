45bbfc6fdc385b55b2b991e0e99fb78b208ecb1f
Gou Furuya <innocent.zero@gmail.com>
2016-01-29 20:38:50 +0900

    Update lita config
    Add redistogo addon settings

--- lita_config.rb
+++ lita_config.rb
@@ -34,4 +34,7 @@ Lita.configure do |config|
   # Slack configuration
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['SLACK_TOKEN']
+
+  config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.http.port   = ENV["PORT"]
 end
